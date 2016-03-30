f3f8a28ad895339053bec5aacd6f738f660500a0
5c5 <colas.p.s@gmail.com>
2016-03-30 15:55:44 +0200

    ajout du cmakelist et des src

---
+++ src/main.cpp
@@ -0,0 +1,23 @@
+/**
+ * GONNOT Alix
+ * PICARD Colas
+ *
+ * Main du projet Analyse
+ * 
+ * Etape 1 : Extraire des objets d'une scene
+ * Etape 2 : Segmentation des membres
+ *
+*/
+#include <stdio.h>
+#include <opencv2/core/core.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+#include <string.h>
+#include <iostream>
+#include <math.h>
+#include <list>
+
+
+int main(int arc, char ** argv){
+	return EXIT_SUCESS;
+}

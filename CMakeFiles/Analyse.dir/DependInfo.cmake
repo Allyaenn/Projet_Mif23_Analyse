9dd9a1cbf94318c992f1dea3fd746adb133f833a
Alix <alix.gonnot@gmail.com>
2016-04-20 15:47:49 +0200

    mise en place des filtres

---
+++ CMakeFiles/Analyse.dir/DependInfo.cmake
@@ -0,0 +1,24 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/alix/Documents/Master/M1/S2/Mif23/Analyse/Projet_Mif23_Analyse/src/main.cpp" "/home/alix/Documents/Master/M1/S2/Mif23/Analyse/Projet_Mif23_Analyse/CMakeFiles/Analyse.dir/src/main.cpp.o"
+  "/home/alix/Documents/Master/M1/S2/Mif23/Analyse/Projet_Mif23_Analyse/src/utils.cpp" "/home/alix/Documents/Master/M1/S2/Mif23/Analyse/Projet_Mif23_Analyse/CMakeFiles/Analyse.dir/src/utils.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/Locals/OpenCV-3.1.0/include"
+  "/usr/local/include/opencv"
+  "/usr/local/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
--- src/utils.cpp
+++ src/utils.cpp
@@ -9,33 +9,35 @@ void usage(){
 Mat spatialSmoothingAvg(Mat image, double lambda)
 {
 	double h;
-	for(int i = 0; i<image.rows; i++)
+	for(int i = 1; i<image.rows; i++)
 	{
-		for(int i = 0; i<image.rows; i++)
+		for(int j = 1; j<image.cols; j++)
 		{
 			//calcul du noyau
-			
-			//calcul de la convolution
+			unsigned char res = 0;
+			for (int u = -1; u < 2; u++)
+			{
+				for (int v = -1; v < 2; v++)
+				{	
+					double h = 1.0/9.0;
+					//cout<<"h : "<<h<<" u : "<<u<<" v :"<<v<<std::endl;
+					res = res + (h*image.at<unsigned char>(i+u,j+v));
+				}
+			} 
+			image.at<unsigned char>(i,j) = res;
 		}
 	}
 	
 	return image;
 }
 
-/*Calcul d'un lissage spatial 
-  noyau = Gaussienne*/
-Mat spatialSmoothingGauss(Mat image, double sigma)
+Mat spatialSmoothingAvgColor(Mat image, double lambda)
 {
-
-	
-	const double pi = 3.14159265358979323846;
-	
+	double h;
 	for(int i = 1; i<image.rows; i++)
 	{
-		for(int j = 1; j < image.cols; j++)
+		for(int j = 1; j<image.cols; j++)
 		{
-			//calcul du noyau
-			//h = (1/(2*pi*pow(sigma,2)))*exp(-(pow(i,2)-pow(j,2))/sigma);
 			for(int k = 0; k < 3; k++)
 			{
 				//calcul de la convolution pour chaque composante couleur
@@ -44,8 +46,6 @@ Mat spatialSmoothingGauss(Mat image, double sigma)
 				{
 					for (int v = -1; v < 2; v++)
 					{	
-						//double h = (1/(2*pi*pow(sigma,2)))*exp((-pow(u,2)-pow(v,2))/(2*sigma));
-						//cout<<"h : "<<h<<" u : "<<u<<" v :"<<v<<std::endl;
 						double h = 1.0/9.0;
 						res = res + (h*(image.at<Vec3b>(i+u,j+v)[k]));
 					}
@@ -54,8 +54,37 @@ Mat spatialSmoothingGauss(Mat image, double sigma)
 			}
 		}
 	}
+	
+	return image;
+}
+
+
+/*Calcul d'un lissage spatial 
+  noyau = Gaussienne*/
+Mat spatialSmoothingGauss(Mat image, double sigma)
+{
+	const double pi = 3.14159265358979323846;
+	
+	for(int i = 1; i<image.rows; i++)
+	{
+		for(int j = 1; j < image.cols; j++)
+		{
+				unsigned char res = 0;
+				for (int u = -1; u < 2; u++)
+				{
+					for (int v = -1; v < 2; v++)
+					{	
+						double h = (1/(2*pi*pow(sigma,2)))*exp((-pow(u,2)-pow(v,2))/(2*sigma));
+						//cout<<"h : "<<h<<" u : "<<u<<" v :"<<v<<std::endl;
+						res = res + (h*(image.at<unsigned char>(i+u,j+v)));
+					}
+				} 
+				image.at<unsigned char>(i,j) = res;
+		}
+	}
 	return image;	
 }
+
 
 //BGR
 
@@ -78,3 +107,4 @@ Mat spatialSmoothingExp(Mat image, double gamma)
 	
 	return image;
 }
+
